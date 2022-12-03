642b195849fa68aa38eb510310662e7f7d5c509a
Layne <Layne@atguigu.com>
2022-12-03 17:52:09 +0800

    push commit

--- src/main/java/com/atguigu/git/gitTest.java
+++ src/main/java/com/atguigu/git/gitTest.java
@@ -8,5 +8,6 @@ public class gitTest {
         System.out.println("hello4");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
